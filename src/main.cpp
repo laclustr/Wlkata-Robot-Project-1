a7c967f516d2c376bb26b4eb184fe85f1c31debe
laclustr <gant.a.vance@gmail.com>
2025-08-02 23:30:59 -0400

    started working on a cps counter; has nothing to do with wlkata but too lazy to change repo name

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define BUTTON_PIN 8
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+
+unsigned long timeSinceLastPress = 0.0;
+int count = 0;
+unsigned long currentMillis;
+String strin;
+
+void updateDisplay(int &count);
+void updateCount(int &count);
+
+void setup(void) {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+  pinMode(BUTTON_PIN, INPUT);
+  u8g2.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  currentMillis = millis();
+  if (currentMillis - timeSinceLastPress > 10000) {
+    count = 0;
+    timeSinceLastPress = 0.0;
+  }
+  updateCount(count);
+  updateDisplay(count);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void updateDisplay(int &count) {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  strin = "Count: " + String(count).c_str() + "; CPS = " + String(count / 10.0, 2).c_str();
+  u8g2.drawStr(0, 10, strin);
+  u8g2.sendBuffer();
 }
+
+void updateCount(int &count) {
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+
+  }
+}
+
